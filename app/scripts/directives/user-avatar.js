38de92a0eb9f2a8ee899151d83c48920b9aa4fa7
Martin Lagrange <lagrangemartin@gmail.com>
2016-02-19 19:37:26 -0800

    Split chat.html into directives

---
+++ app/scripts/directives/user-avatar.js
@@ -0,0 +1,13 @@
+angular.module('app').directive('userAvatar', function() {
+  return {
+    restrict: "E",
+    template: `<img src="{{avatarUrl}}" alt="{{uuid}}" class="circle">`,
+    scope: {
+      uuid: "@",
+    },
+    controller: function($scope){
+      // Generating a uniq avatar for the given uniq string provided using robohash.org service
+      $scope.avatarUrl = '//robohash.org/' + $scope.uuid + '?set=set2&bgset=bg2&size=70x70';
+    }
+  };
+});
