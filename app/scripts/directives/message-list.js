c9c107e9ecf7c6945ce11c76e123bc109cfd4e21
Martin Lagrange <lagrangemartin@gmail.com>
2016-02-21 19:19:57 -0800

    Enable an autoScroll indicator

--- app/scripts/directives/message-list.js
+++ app/scripts/directives/message-list.js
@@ -9,11 +9,38 @@ angular.module('app').directive('messageList', function($timeout) {
     link: function(scope, element, attrs, ctrl) {
 
       var scrollToBottom = function() {
-          list = angular.element(element)[0]
+          var list = angular.element(element)[0]
           list.scrollTop = list.scrollHeight;
         };
+
+      var scrollReachedBottom = function(){
+        element = angular.element(element)
+        return $(element).scrollTop() + $(element).innerHeight() >= $(element)[0].scrollHeight
+      }
+
+      var updateScrollStatus = function() {
+
+        var previousValue = scope.autoScrollDown;
+        var newValue = scrollReachedBottom();
+
+        // Update the autoScrollDown value if it has changed
+        if (newValue != previousValue){
+          scope.autoScrollDown = newValue ;
+          scope.$digest();
+        }
+      };
+
+      // Scroll down when the list is rendered
       $timeout(scrollToBottom, 5);
-      
+
+      // Check where the scroll is on the list
+       angular.element(element).bind("scroll", updateScrollStatus);
+
+
+    },
+    controller: function($scope){
+      // Auto scroll down is acticated when first loaded
+      $scope.autoScrollDown = true;
     }
   };
 });
