10de931db98d03c233c2bd30fc571b4d8ea5ef6a
Martin Lagrange <lagrangemartin@gmail.com>
2016-02-21 17:56:01 -0800

    Scroll Down when the messages are first loaded

--- app/scripts/directives/message-list.js
+++ app/scripts/directives/message-list.js
@@ -5,6 +5,15 @@ angular.module('app').directive('messageList', function($timeout) {
     templateUrl: '/templates/directives/message-list.html',
     scope: {
       messages: "=",
+    },
+    link: function(scope, element, attrs, ctrl) {
+
+      var scrollToBottom = function() {
+          list = angular.element(element)[0]
+          list.scrollTop = list.scrollHeight;
+        };
+      $timeout(scrollToBottom, 5);
+      
     }
   };
 });
