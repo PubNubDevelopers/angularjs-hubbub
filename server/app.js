6d3148265cf7ea79dfb64e2a33931f1030f557ef
Martin Lagrange <lagrangemartin@gmail.com>
2016-04-01 12:04:38 -0700

    Implement Github Oauth authentification - authorization code request on the client and middleware on the server to request access_token

--- server/app.js
+++ server/app.js
@@ -1,29 +1,25 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var github_oauth = require('./routes/github_oauth');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(cors());
+
+app.use('/', github_oauth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
