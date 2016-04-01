4be3b753123b9b5171cf6865924b871ddcc20b03
Martin Lagrange <lagrangemartin@gmail.com>
2016-04-01 15:23:05 -0700

    Routes are requiring to be authenticated

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -1,12 +1,25 @@
 angular
   .module('app')  
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $authProvider) {
+ 
+    // Redirect to the login page if not authenticated
+    var requireAuthentication = function ($q, $location, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      } else {
+        $location.path('/login');
+      }
+      return deferred.promise;
+    };
+
     $routeProvider
       .when('/', {
-        templateUrl: 'views/chat.html'
+        templateUrl: 'views/chat.html',
+        resolve: { requireAuthentication: requireAuthentication }
       })
       .when('/login', {
-        templateUrl: 'views/login.html',
+        templateUrl: 'views/login.html'
       })
       .otherwise({
         redirectTo: '/'
