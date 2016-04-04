b50ca2b1973cc43c9a0b67a38c917e69d323ab01
Martin Lagrange <lagrangemartin@gmail.com>
2016-04-04 11:15:15 -0700

    Merge files in app.js

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var http = require("http");
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var request = require('request');
+var qs = require('querystring');
 var Datastore = require('nedb')
-
-var github_oauth = require('./routes/github_oauth');
 
 var app = express();
 
@@ -16,7 +18,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'ejs');
 
 app.use(cors());
 
@@ -37,7 +38,31 @@ db.access_tokens = new Datastore({ filename: 'db/access_tokens.db', autoload: tr
  |--------------------------------------------------------------------------
 */
 
-app.use('/', github_oauth);
+  app.post('/auth/github', function(req, res) {
+
+    var accessTokenUrl = 'https://github.com/login/oauth/access_token';
+
+    var params = {
+      code: req.body.code,
+      client_id: req.body.clientId,
+      client_secret: '3c69fde2d90e34e6ccc7eafd5920bf51d0f540e0',
+      redirect_uri: req.body.redirectUri
+    };
+
+    // Exchange authorization code for access token.
+    request.post({ url: accessTokenUrl, qs: params }, function(err, response, token) {
+
+         token = qs.parse(token);
+         accessToken = token.accessToken;
+
+         res.send({token: token.access_token});
+    });
+  });
+
+
+/*
+ |--------------------------------------------------------------------------
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,8 +77,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || 500).send({
       message: err.message,
       error: err
     });
@@ -63,8 +87,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+  res.status(err.status || 500).send({
     message: err.message,
     error: {}
   });
