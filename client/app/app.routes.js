0afb8613eabe014b4d307b47df100ee6c9c6723d
Martin Lagrange <lagrangemartin@gmail.com>
2016-04-14 21:01:56 -0700

    Cleanup

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -3,7 +3,7 @@ angular
   .config(function ($routeProvider, $authProvider) {
  
     // Redirect to the login page if not authenticated
-    var requireAuthentication = function ($q, $location, $auth, AuthenticationService) {
+    var requireAuthentication = function ($location, $auth, AuthenticationService) {
 
       if ($auth.isAuthenticated()) {
         return AuthenticationService.login()
@@ -27,22 +27,14 @@ angular
         template: null,
         controller: function(AuthenticationService, $location, ngNotify){          
               
-            AuthenticationService.logout().then(function(){
-                
-                $location.path('/login');
-
-            }).catch(function(error) {
-
+            AuthenticationService.logout().catch(function(error) {
               // The logging out process failed on the server side
               if(error.status == 500){
-                ngNotify.set('Logout failed.', { type: 'error' });
-                $location.path('/login');
-              } 
-              else{
-                $location.path('/login');
-              }
-
-          });
+                ngNotify.set('Logout failed.', { type: 'error' }); 
+              }    
+            }).finally(function(){
+              $location.path('/login');
+            });
             
         }
       })
