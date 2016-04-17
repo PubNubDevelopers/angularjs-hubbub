f9e79ceca311104089a71919322b808e8b04ff16
Martin Lagrange <lagrangemartin@gmail.com>
2016-04-17 13:00:10 -0700

    Fix grunt build issues

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/lodash/lodash.js',
       'bower_components/ng-notify/src/scripts/ng-notify.js',
       'bower_components/satellizer/satellizer.js',
+      'bower_components/particles.js/particles.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
