d0d7555aa6685b9432e0ccc32d1dfdcb48cd77ba
Martin Lagrange <lagrangemartin@gmail.com>
2016-04-24 23:31:51 -0700

    feat(Remove the user menu from the online user list)

--- client/app/components/online-user-list/online-user-list.directive.js
+++ client/app/components/online-user-list/online-user-list.directive.js
@@ -4,23 +4,9 @@ angular.module('app').directive('onlineUserList', function($rootScope, OnlineUse
     replace: true,
     templateUrl: 'components/online-user-list/online-user-list.html',
 
-    link: function(scope, element, attrs) {
-
-    	$('.dropdown-button').dropdown({
-	      inDuration: 300,
-	      outDuration: 225,
-	      constrain_width: false, // Does not change width of dropdown to that of the activator
-	      hover: true, // Activate on hover
-	      gutter: 0, // Spacing from edge
-	      belowOrigin: true, // Displays dropdown below the button
-	      alignment: 'left' // Displays dropdown with edge aligned to the left of button
-	    });
-	    
-   	},
-    controller: function($scope, currentUser){
+    controller: function($scope){
       
       $scope.users = OnlineUsersService.getOnlineUsers();
-      $scope.currentUser = currentUser.get();
     }
   };
 });
