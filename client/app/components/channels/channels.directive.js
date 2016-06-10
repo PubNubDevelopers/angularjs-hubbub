3b1749b3174ec3ff61ceff34bf6e3889ae649358
Martin Lagrange <lagrangemartin@gmail.com>
2016-06-10 10:44:16 -0700

    Update channels component to display channels from the ConversationList service

--- client/app/components/channels/channels.directive.js
+++ client/app/components/channels/channels.directive.js
@@ -2,7 +2,12 @@ angular.module('app').directive('channels', function() {
   return {
     restrict: "E",
     templateUrl: 'components/channels/channels.html',
+    controller: function($scope, ConversationList){
 
+    	ConversationList.all().then(function(conversations){
+    		$scope.channels = conversations
+    	})
 
+   	}
   };
 });
