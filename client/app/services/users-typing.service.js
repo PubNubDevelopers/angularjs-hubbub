1bbe50bee553a5283fc0e555539c4d83c1e705d9
Martin Lagrange <lagrangemartin@gmail.com>
2016-06-10 11:01:20 -0700

    Update the Typing indicator to be bound to each conversation

---
+++ client/app/services/users-typing.service.js
@@ -0,0 +1,43 @@
+angular.module('app')
+.factory('UsersTyping', ['$rootScope', 'Pubnub', 'currentUser', '$pubnubChannelGroup',
+ function UsersTyping($rootScope, Pubnub, currentUser,$pubnubChannelGroup) {
+    
+    var channelGroup = 'conversations_' + currentUser.get().id.toString()+'-pnpres';
+
+
+
+    var options = {
+
+      channelExtension:  {
+
+        $$store: function(event) {
+          
+        // We don't want to receive our own presence events
+        if(event['uuid'].toString() === currentUser.get().id.toString()) return;
+
+        // Add people typing
+        if(event['action'] === 'state-change' && event['data']['isTyping']){
+
+            // Check if not already in the array
+            if(!_.find(this.$messages, { uuid: event['uuid']}))
+              console.log('Adding', event['uuid'])
+              this.$messages.push({uuid: event['uuid']});
+
+        }
+        // Remove people typing
+        else if(  ( event['action'] === 'state-change' && event['data']['isTyping'] === false ) || 
+                    event['action'] === 'timeout'                                               || 
+                    event['action'] === 'leave' ){
+
+            _.remove(this.$messages, function(user) {
+              return user['uuid'] === event['uuid'];
+            });
+        }
+
+        }
+      }
+    }
+
+    return $pubnubChannelGroup(channelGroup,options);
+}
+]);
