9f1e539afc3b682e9ea4bbc2d15e445b55cf7cba
Martin Lagrange <lagrangemartin@gmail.com>
2016-06-13 17:14:33 -0700

    Add notification messages

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/ngToast/dist/ngToast.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-loading-bar/build/loading-bar.js',
+      'bower_components/push.js/push.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
